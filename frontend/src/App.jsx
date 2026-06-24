cddab6046352b1e3c8e0db0e4c710e05b0bed11f
Dasun53 <dasununimail@gmail.com>
2026-06-24 19:56:18 +0530

    Adding hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/HeroSection";
 
 const App = () => {
   return (
     <div>
       <div className="container" style={{ padding: "10px" }}>
         <Navbar />
+        <main>
+          <Hero />
+        </main>
       </div>
     </div>
   );
