efb419fbe5b911c7ff4079d29bee1d81702ed83f
Dasun53 <dasununimail@gmail.com>
2026-07-26 23:06:55 +0530

    back button fix

--- frontend/src/pages/GameDetails.jsx
+++ frontend/src/pages/GameDetails.jsx
@@ -6,12 +6,6 @@ import { Star, ShoppingCart, ArrowLeft, Shield } from "lucide-react";
 import "./CSS/GameDetails.css";
 
 export default function GameDetails() {
-  const { id } = useParams();
-  const [game, setGame] = useState(null);
-  const [library, setLibrary] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const { addToCart, cart } = useCart();
-  const { token } = useAuth();
   const navigate = useNavigate();
 }
 
